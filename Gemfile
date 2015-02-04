b292b2866b06e492cdcaea1d4bf79f0473fdc18f
Florent Espanet <florent.esp@gmail.com>
2015-02-04 23:29:40 +0100

    Add basic style and static_pages.

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 
 gem 'devise'
+gem 'bootstrap-sass', '~> 3.3.3'
 
 gem 'jquery-rails'
 gem 'turbolinks'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'contact' => 'static_pages#contact'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+
   devise_for :users
   resources :feeds
 
-  root 'feeds#index'
+  root 'static_pages#home'
 end
