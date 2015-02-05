d02c4285008b0dfd711c1055b9bb6611747ddbe0
Florent Espanet <florent.esp@gmail.com>
2015-02-05 22:27:57 +0100

    Add dashboard.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
+  get 'dashboard' => 'dashboard#view'
 
   devise_for :users
   resources :feeds
